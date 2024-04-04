4a9e0a6fabf19f0250dc200dc9a938f90bb94bc0
San7o <santigio2003@gmail.com>
2024-04-04 11:15:24 +0200

    Added profile page

---
+++ src/main/java/Profile.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+// Profile page, should be accessible only
+// when logged in, It shows the session Attributes
+public class Profile extends HttpServlet {
+  public void doGet (HttpServletRequest req,
+                     HttpServletResponse res)
+    throws ServletException, IOException
+  {
+
+
+    // Get Session Attributes
+    
+    // Do no create a new session
+    HttpSession session = req.getSession(true); 
+    String name = (String) session.getAttribute("name");
+
+
+    // Write output
+    PrintWriter out = res.getWriter();
+    out.println("Hello " + name + ", welcome to your profile page!");
+    out.close();
+  }
+}
