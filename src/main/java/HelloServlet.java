bc974b6cfb5c367187e421bd792d11723da36bcc
Qw3rty26 <asarovalerio26@gmail.com>
2024-04-05 10:16:06 +0200

    Fixes #16: Deleted DynamicPage class and implemented the include method of RequestDispatcher

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -5,11 +5,9 @@ import javax.servlet.*;
 
 public class HelloServlet extends HttpServlet {
 	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    		PrintWriter out = response.getWriter();
-		DynamicPage printer = new DynamicPage();
-		printer.printPage(out, "/hello-world.html");
-		out.close();
-	}
+    		RequestDispatcher dispatcher = request.getRequestDispatcher("/Homepage.html");
+		dispatcher.include(request,response);
+ 	}
 
 	public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
