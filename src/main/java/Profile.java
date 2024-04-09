4a5300b9cae0e3369746e8a2a5ab3a4918050a8d
San7o <santigio2003@gmail.com>
2024-04-09 15:02:55 +0200

    Added synchronized for Thread safety

--- src/main/java/Profile.java
+++ src/main/java/Profile.java
@@ -6,6 +6,8 @@ import javax.servlet.*;
 // Profile page, should be accessible only
 // when logged in, It shows the session Attributes
 public class Profile extends HttpServlet {
+
+  @Override
   public void doGet (HttpServletRequest req,
                      HttpServletResponse res)
     throws ServletException, IOException
@@ -15,13 +17,17 @@ public class Profile extends HttpServlet {
     // Get Session Attributes
     
     // Do no create a new session
-    HttpSession session = req.getSession(false); 
-    String name = (String) session.getAttribute("name");
+    HttpSession session = req.getSession(false);
+    
+    // Thread Safe
+    synchronized(session) {
 
+        String name = (String) session.getAttribute("name");
 
-    // Write output
-    PrintWriter out = res.getWriter();
-    out.println("Hello " + name + ", welcome to your profile page!");
-    out.close();
+        // Write output
+        PrintWriter out = res.getWriter();
+        out.println("Hello " + name + ", welcome to your profile page!");
+        out.close();
+    }
   }
 }
