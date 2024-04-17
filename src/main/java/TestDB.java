195f02e7d72d955c31f23841b7e55904820d7be1
San7o <santigio2003@gmail.com>
2024-04-17 14:31:46 +0200

    Moved driver to init

--- src/main/java/TestDB.java
+++ src/main/java/TestDB.java
@@ -12,49 +12,52 @@ import java.sql.*;
 // - ERR otherwise, with the error message
 public class TestDB extends HttpServlet {
 
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+
+    // Loading the driver
+    try {
+
+      Class.forName("org.apache.derby.jdbc.ClientDriver");
+    }
+    catch (ClassNotFoundException e) {
+
+      throw new ServletException("Driver not found");
+    }
+  }
+
   public void doGet (HttpServletRequest req,
                      HttpServletResponse res)
     throws ServletException, IOException
   {
     
-    try {
+      try {
 
-        // Loading the driver
-        Class.forName("org.apache.derby.jdbc.ClientDriver");
+        // Connecting
+        String url = "jdbc:derby://localhost:1527/DemoDB";
+        Connection con = DriverManager.getConnection(url);
 
-        try {
-            // Connecting
-            String url = "jdbc:derby://localhost:1527/DemoDB";
-             Connection con = DriverManager.getConnection(url);
+        // Query
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM users");
 
-            // Query
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+        // Output
+        PrintWriter out = res.getWriter();
 
-            // Output
-            PrintWriter out = res.getWriter();
+        while (rs.next()) {
 
-            while (rs.next()) {
-
-                String s = rs.getString("NAME");
-                out.println(s);
-            }
-
-            out.close();
-         }
-         catch (SQLException e) {
-
-            PrintWriter out = res.getWriter();
-            out.println("ERR " + e.getMessage());
-            out.close();
+            String s = rs.getString("NAME");
+            out.println(s);
         }
-    }
-    catch(ClassNotFoundException e) {
+
+        out.close();
+      }
+      catch (SQLException e) {
 
         PrintWriter out = res.getWriter();
         out.println("ERR " + e.getMessage());
         out.close();
-    }
-    
+      }
   }
 }
